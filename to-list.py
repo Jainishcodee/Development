a531ecfceaefa27219c119e0f6ebbcaecbbfcfbc
Jainishcodee <85493803+Jainishcodee@users.noreply.github.com>
2023-01-19 19:12:46 +0530

    Update to-list.py

--- to-list.py
+++ to-list.py
@@ -1,51 +1,32 @@
+import eel
+import json
+
 tasks = []
-def display_tasks():
-    print("Here are your tasks:")
-    for i, task in enumerate(tasks):
-        print(f"{i+1}: {task}")
 
-
-def add_task(task):
+@eel.expose
+def add_task(name, status, id):
+    task = {"name": name, "status": status, "id": id}
     tasks.append(task)
-    print(f"Task '{task}' added to your to-do list")
 
+@eel.expose
+def edit_task(id, name, filter):
+    tasks[id]["name"] = name
+    eel.showTodo(filter)()
 
-def remove_task(index):
-    del tasks[index]
-    print("Task removed")
+@eel.expose
+def delete_task(id, filter):
+    tasks.pop(id)
+    eel.showTodo(filter)()
 
+@eel.expose
+def mark_completed(id, status):
+    tasks[id]["status"] = "completed" if status else "pending"
 
-def complete_task(index):
-    tasks[index] = f"{tasks[index]} (Completed)"
-    print("Task marked as completed")
+@eel.expose
+def clear_list():
+    tasks.clear()
+    eel.showTodo("all")()
 
-
-def menu():
-    while True:
-        display_tasks()
-        print("""
-Menu:
-  1. Add task
-  2. Remove task
-  3. Complete task
-  4. Quit
-""")
-        choice = input("Enter a choice: ")
-
-        if choice == "1":
-            task = input("Enter a task: ")
-            add_task(task)
-        elif choice == "2":
-            index = int(input("Enter the index of the task to remove: ")) - 1
-            remove_task(index)
-        elif choice == "3":
-            index = int(
-                input("Enter the index of the task to mark as completed: ")) - 1
-            complete_task(index)
-        elif choice == "4":
-            break
-        else:
-            print("Invalid choice")
-
-
-menu()
+eel.init('DSA/rough')
+# Start the Eel web server
+eel.start('index.html', mode='brave')
